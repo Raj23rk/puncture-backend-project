5fb5fc563a49a91449a1f6ea560e3a20b6ebc94a
Rajkumar A <kumarrk23dev@gmail.com>
2025-07-06 20:37:48 +0530

    user schema added

--- models/User.js
+++ models/User.js
@@ -63,4 +63,5 @@ userSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+
 module.exports = mongoose.model('User', userSchema);
